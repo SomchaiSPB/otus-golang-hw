9b7041bb00a55d19e5565e6ecc70d3435f09e400
Denis Gaivoronskii <denis.gaivoronskii@helastel.com>
2021-09-08 10:37:45 +0300

    basic function for unpack completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,48 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if len(s) == 0 {
+		return "", nil
+	}
+	if !unicode.IsLetter(rune(s[0])) {
+		return "", ErrInvalidString
+	}
+
+	var result string
+	var prev rune
+
+	for i, cur := range s {
+		if i < 1 {
+			prev = cur
+			result += string(cur)
+			continue
+		}
+
+		if !unicode.IsLetter(prev) && !unicode.IsLetter(cur) {
+			return "", ErrInvalidString
+		}
+
+		if !unicode.IsLetter(cur) {
+			cur, _ := strconv.ParseInt(string(cur), 10, 32)
+			if cur == 0 {
+				result = result[:i-1]
+				continue
+			}
+			result += strings.Repeat(string(prev), int(cur) - 1)
+		} else {
+			result += string(cur)
+		}
+
+		prev = cur
+	}
+
+	return result, nil
 }
