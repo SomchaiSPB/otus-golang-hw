db59b7ae2a6f072421d381ca712cf644beb3c338
Denis Gaivoronskii <denis.gaivoronskii@helastel.com>
2021-09-11 18:10:00 +0300

    unpack function complete with slashes

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -17,33 +17,61 @@ func Unpack(s string) (string, error) {
 		return "", ErrInvalidString
 	}
 
+	runeArr := s
+
 	var result string
 	var prev rune
+	isEscaped := false
+	slashCounter := 0
 
-	for i, cur := range s {
+	for i, cur := range runeArr {
 		if i < 1 {
 			prev = cur
 			result += string(cur)
 			continue
 		}
-
-		if !unicode.IsLetter(prev) && !unicode.IsLetter(cur) {
+		if unicode.IsNumber(prev) && unicode.IsNumber(cur) && !isEscaped {
 			return "", ErrInvalidString
 		}
 
-		if !unicode.IsLetter(cur) {
-			cur, _ := strconv.ParseInt(string(cur), 10, 32)
-			if cur == 0 {
+		if isSlash(prev) && unicode.IsNumber(cur) && slashCounter % 2 == 1 {
+			isEscaped = true
+		}
+
+		if unicode.IsNumber(cur) {
+			if cur == 48 {
 				result = result[:i-1]
 				continue
 			}
-			result += strings.Repeat(string(prev), int(cur) - 1)
+
+			if isSlash(prev) && isEscaped {
+				result += string(cur)
+			} else {
+				cur, _ := strconv.ParseInt(string(cur), 10, 32)
+				result += strings.Repeat(string(runeArr[i-1]), int(cur) - 1)
+			}
+		} else if isSlash(cur) {
+			slashCounter++
+			if !isSlash(prev) {
+				prev = cur
+				continue
+			}
+			if isSlash(prev) && slashCounter %2 == 0 {
+				result += string(cur)
+				prev = cur
+				continue
+			}
 		} else {
 			result += string(cur)
+			slashCounter = 0
 		}
 
 		prev = cur
 	}
 
 	return result, nil
+}
+
+func isSlash(r rune) bool {
+	return r == 92
 }
