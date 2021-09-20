63425b4547740869deedeab407470411a435b295
Denis Gaivoronskii <denis.gaivoronskii@helastel.com>
2021-09-20 20:11:41 +0300

    top rate words task complete without asterix

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -14,7 +14,7 @@ type List []FrequentWords
 
 func (l List) Less(i int, j int) bool {
 	if l[i].Value == l[j].Value {
-		return l[i].Key < l[j].Key
+		return l[i].Key > l[j].Key
 	}
 	return l[i].Value < l[j].Value
 }
@@ -27,33 +27,29 @@ func (l List) Swap(i int, j int) {
 	l[i], l[j] = l[j], l[i]
 }
 
-func (l List) ToStringArray() []string {
+func (l List) ToStringArray(size int) []string {
 	output := make([]string, 0)
 
 	for _, val := range l {
 		output = append(output, val.Key)
 	}
 
-	return output
+	return output[:size]
 }
 
 func Top10(input string) []string {
+	if len(input) == 0 {
+		return []string{}
+	}
+
 	words := strings.Fields(input)
 	tempMap := make(map[string]int)
-    var	sortedResult []string
 
 	for _, word := range words {
 		tempMap[word] = tempMap[word] + 1
 	}
 
-	tempResult := rankWordTop(tempMap)
-	tempIntArr := make(map[int][]string)
-
-	for key, val := range tempResult {
-
-	}
-
-	return tempResult.ToStringArray()[:10]
+	return rankWordTop(tempMap).ToStringArray(10)
 }
 
 func rankWordTop(dict map[string]int) List {
