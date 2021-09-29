f775ae5525c049be92c44b968d1b33d2f5d279e2
Denis Gaivoronskii <denis.gaivoronskii@helastel.com>
2021-09-29 11:27:47 +0300

    parallel tests are ok. mutex working

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 func (k Key) String() string {
@@ -13,7 +15,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	// mu       sync.Mutex
+	mu       sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -33,13 +35,13 @@ func NewCache(capacity int) Cache {
 }
 
 func (c *lruCache) Set(key Key, value interface{}) bool {
-	// c.mu.Lock()
-	// defer c.mu.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	newCacheItem := &cacheItem{
 		key:   key.String(),
 		value: value,
 	}
-	if _, ok := c.Get(key); ok {
+	if _, ok := c.items[key]; ok {
 		c.items[key].Value = newCacheItem
 
 		return true
@@ -59,8 +61,8 @@ func (c *lruCache) Set(key Key, value interface{}) bool {
 }
 
 func (c *lruCache) Get(key Key) (interface{}, bool) {
-	// c.mu.Lock()
-	// defer c.mu.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	listNode, ok := c.items[key]
 
 	if !ok {
@@ -73,8 +75,8 @@ func (c *lruCache) Get(key Key) (interface{}, bool) {
 }
 
 func (c *lruCache) Clear() {
-	// c.mu.Lock()
-	// defer c.mu.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.items = make(map[Key]*ListItem, c.capacity)
 	c.queue = NewList()
 }
