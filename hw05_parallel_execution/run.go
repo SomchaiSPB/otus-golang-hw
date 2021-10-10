52c33bbf9993aa69dd5a6304a0c3530dd2483547
Denis Gaivoronskii <denis.gaivoronskii@helastel.com>
2021-10-10 13:02:48 +0300

    parallel execution complete. Tests ok, Race test ok

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,74 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type counter struct {
+	sync.Mutex
+	errCount int
+}
+
+func (c *counter) increment() {
+	c.Lock()
+	defer c.Unlock()
+	c.errCount++
+}
+
+func (c *counter) decrement() {
+	c.Lock()
+	defer c.Unlock()
+	c.errCount--
+}
+
+var wg sync.WaitGroup
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	var in = make(chan Task, len(tasks))
+	out := make(chan struct{}, 1)
+	counter := new(counter)
+
+	for _, task := range tasks {
+		in <- task
+	}
+	close(in)
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for task := range in {
+				if counter.errCount >= m {
+					select {
+					case out <- struct{}{}:
+						close(out)
+					}
+				}
+				err := task()
+				if err != nil {
+					counter.increment()
+				}
+				select {
+				case <-out:
+					return
+				default:
+					//
+				}
+			}
+		}()
+	}
+	wg.Wait()
+
+	select {
+	case <-out:
+		return ErrErrorsLimitExceeded
+	default:
+		return nil
+	}
+
 }
