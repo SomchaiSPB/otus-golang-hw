b412fb857718c1a0d8a7f1fda9953b812f1a51cc
Denis Gaivoronskii <denis.gaivoronskii@helastel.com>
2021-10-31 20:49:18 +0300

    main go updated with copy

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -10,45 +11,82 @@ import (
 var (
 	ErrUnsupportedFile       = errors.New("unsupported originalFile")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds originalFile size")
-	originalFile os.File
-	targetFile os.File
-
+	ErrorOpenFile = errors.New("file open failed")
+	ErrorreateFile = errors.New("file create failed")
+	originalFile *os.File
+	targetFile *os.File
+	tmpFile *os.File
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	originalFile, err := os.Open(fromPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			// Not found
+	originalFile, ErrorOpenFile = os.Open(fromPath)
+	if ErrorOpenFile != nil {
+		if os.IsNotExist(ErrorOpenFile) {
+			return ErrorOpenFile
+		}
+	}
+	targetFile, ErrorreateFile = os.Create(toPath)
+	if ErrorreateFile != nil {
+		if os.IsNotExist(ErrorreateFile) {
+			return ErrorreateFile
+		}
+	}
+	defer originalFile.Close()
+	defer targetFile.Close()
+
+	// Create tmp filename from original fileinfo
+
+	//fileInfo := os.FileInfo()
+	//
+	//// TODO finish this
+	//if offset > os.FileInfo().Size() {
+	//	return ErrOffsetExceedsFileSize
+	//}
+
+	if limit > 0 {
+		_, err := io.CopyN(targetFile, originalFile, limit)
+		if err != nil {
+			log.Panicf("failed to read original file: %v", err)
 		}
 	}
 
-	targetFile, err := os.Create(toPath)
 
-	buf := make([]byte, limit)
+	if offset == 0 && limit == 0 {
 
-	// Check offset and limit
-	for offset < limit {
-		read, err := originalFile.Read(buf[offset:])
+		//read, err := io.ReadAll(originalFile)
+		originalFile, err := ioutil.ReadFile(fromPath)
 
-		offset += int64(read)
-
-		if err == io.EOF {
-			break
-		}
 		if err != nil {
 			log.Panicf("failed to read original file: %v", err)
 		}
 
+		//written, err := targetFile.Write(originalFile)
+		err = ioutil.WriteFile(toPath, originalFile, 0644)
+		if err!= nil {
+			log.Panicf("failed to write: %v", err)
+		}
+
+	} else {
+		buf := make([]byte, limit)
+		for offset < limit {
+			read, err := originalFile.Read(buf[offset:])
+
+			offset += int64(read)
+
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Panicf("failed to read original file: %v", err)
+			}
+		}
+
+		_, err := targetFile.Write(buf)
+		if err!= nil {
+			log.Panicf("failed to write: %v", err)
+		}
 
 	}
-
-
-
-	defer originalFile.Close()
-	defer targetFile.Close()
-
 
 	return nil
 }
