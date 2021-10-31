a94d5a26d654b312b2d231b539e5d7274639daac
Denis Gaivoronskii <denis.gaivoronskii@helastel.com>
2021-10-31 18:18:30 +0300

    stages complete according to solve video

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,43 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+
+	for _, stage := range stages {
+		if stage == nil {
+			continue
+		}
+		wrapped := wrap(in, done)
+		in = stage(wrapped)
+	}
+
+	return in
+}
+
+func wrap(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer func() {
+			close(out)
+			for range in {
+				// skip
+			}
+		}()
+		for {
+			select {
+			case <-done:
+				return
+			case data, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- data
+			}
+		}
+	}()
+
+	return out
 }
