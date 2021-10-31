58c4e55a0aa156e96485ec21a967395bcbb64f3e
Denis Gaivoronskii <denis.gaivoronskii@helastel.com>
2021-10-31 19:24:32 +0300

    copy func started WIP

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,53 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"os"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrUnsupportedFile       = errors.New("unsupported originalFile")
+	ErrOffsetExceedsFileSize = errors.New("offset exceeds originalFile size")
+	originalFile os.File
+	targetFile os.File
+
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here.
+	originalFile, err := os.Open(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// Not found
+		}
+	}
+
+	targetFile, err := os.Create(toPath)
+
+	buf := make([]byte, limit)
+
+	// Check offset and limit
+	for offset < limit {
+		read, err := originalFile.Read(buf[offset:])
+
+		offset += int64(read)
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Panicf("failed to read original file: %v", err)
+		}
+
+
+	}
+
+
+
+	defer originalFile.Close()
+	defer targetFile.Close()
+
+
 	return nil
 }
